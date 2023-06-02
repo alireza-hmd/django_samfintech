eeab7d200660334c5f1f6dbf702c92e57eeca3b9
Alireza Hamedi <alireza12196@gmail.com>
2023-06-02 16:50:02 +0330

    task 1 (price updater) completed

---
+++ python/price_update_service/updater.py
@@ -0,0 +1,41 @@
+import csv
+import redis
+import json
+
+REDIS_CONTAINER = 'redis_redis_1'
+r = redis.Redis(host=REDIS_CONTAINER, port=6379, db=0)
+
+stock_price_list = {
+    'stock1': {},
+    'stock2': {},
+    'stock3': {},
+}
+
+with open('price_data.csv') as data_file:
+    stocks_data = csv.reader(data_file, delimiter=',')
+    line_count = 0
+    for row in stocks_data:
+        if line_count == 0:
+            line_count += 1
+        else:
+            time = row[0][:2] + ':' + row[0][2:4]
+            if stock_price_list[row[1]].get(time):
+                stock_price_list[row[1]][time][0] += int(row[2])
+                stock_price_list[row[1]][time][1] += 1
+            else:
+                stock_price_list[row[1]][time] = [int(row[2]), 1]
+
+
+for stock in stock_price_list:
+    stock_item = json.loads(r.get(stock))
+    for time in stock_price_list[stock]:
+        total_price, count = stock_price_list[stock][time]
+        if time in stock_item['time']:
+            index = stock_item['time'].index(time)
+            stock_item['price'][index] = total_price/count
+        else:
+            stock_item['time'].append(time)
+            stock_item['price'].append(total_price/count)
+
+    r.set(stock, json.dumps(stock_item))
+    print(r.get(stock), '\n')
